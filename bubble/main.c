a09ffef4bdcef80fb8fb9b394d0f4e50cb227edf
Aiswarya C <caiswarya645@gmail.com>
2024-12-02 22:04:48 +0530

    Add files via upload

---
+++ bubble/main.c
@@ -0,0 +1,41 @@
+
+#include <stdio.h>
+
+
+int main()
+{
+	int n,i,j,temp;
+	printf("entre the count:");
+	scanf("%d",&n);
+	int arr[5];
+	printf("enter the elments:");
+	for(i=0; i<n; i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+
+	for(i=0; i<n; i++)
+	{
+		for(j=0; j<n-i-1; j++)
+		{
+			if(arr[j]>arr[j+1])
+			{
+				temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+
+
+			}
+		}
+
+
+	}
+
+	printf("elements:");
+	for(i=0; i<n; i++)
+	{
+		printf("%d",arr[i]);
+	}
+
+	return 0;
+}
